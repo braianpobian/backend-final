becf911169318f65cfc1ae4702c4e4b778ff67ba
braian <braiann508@gmail.com>
2025-12-10 21:38:00 -0300

    ruta usuarios y cursos agregar

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("firebase.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 app =FastAPI()
 @app.get("/health")
 def obtener_salud_sitio():
-    return "ok"
+    return "braian"
+
+@app.get("/usuarios")
+def obtener_usuarios_sitio():
+    collection = db.collection("usuarios").stream()
+    return [doc.to_dict() for doc in collection]
+
+
+
+@app.get("/cursos")
+def obtener_cursos_sitio():
+    collection = db.collection("cursos").stream()
+    return [doc.to_dict() for doc in collection]
+
+
